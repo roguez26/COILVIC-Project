0117b5c6824494cb5b16c9396352b4988c4875e4
ivanr <ivanr@roguez>
2024-03-17 14:46:43 -0600

    Inicio de proyecto

---
+++ src/mx/fei/coilvicapp/logic/representante/IInstitutionalRepresentative.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mx.fei.coilvicapp.logic.representante;
+
+/**
+ *
+ * @author ivanr
+ */
+public interface IInstitutionalRepresentative {
+    public int insertInstitutionalRepresentative(InstitutionalRepresentative instutionalRepresentative);
+    
+
+}
